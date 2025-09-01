185a4d8c0c38a9b416c3322aaaf17c21241f59c3
Michal <michalgradzik1000@gmail.com>
2025-09-01 15:09:13 +0200

    Poprawiono kod html, css i js

--- src/js/app.js
+++ src/js/app.js
@@ -1,12 +1,28 @@
 function showContent(itemId) {
     const div = document.getElementById((itemId + "_container"));
     const upper_div = document.getElementById((itemId));
+
     if (div.style.height === '0px' || div.style.height === ""){
         div.style.height = "auto";
-        upper_div.getElementsByTagName("h3").item(0).innerText = itemId + " " + ("\u2BC5".toWellFormed())
+        div.style.margin = "auto auto 3px";
+        div.style.padding = "0 5px 5px";
+        upper_div.style.margin = "auto";
+        if (upper_div.getElementsByTagName("h3").length > 0) {
+            upper_div.getElementsByTagName("h3").item(0).innerText = itemId + " " + ("\u2BC5".toWellFormed())
+        }else if (upper_div.getElementsByTagName("h4").length > 0) {
+            upper_div.getElementsByTagName("h4").item(0).innerText = itemId + " " + ("\u2BC5".toWellFormed())
+        }
     } else {
         div.style.height = "0";
-        upper_div.getElementsByTagName("h3").item(0).innerText = itemId + " " + ("\u2BC6".toWellFormed())
+        div.style.margin = "0";
+        upper_div.style.margin = "3px";
+        div.style.padding = "0";
+        if (upper_div.getElementsByTagName("h3").length > 0) {
+            upper_div.getElementsByTagName("h3").item(0).innerText = itemId + " " + ("\u2BC6".toWellFormed());
+        }else if (upper_div.getElementsByTagName("h4").length > 0) {
+            upper_div.getElementsByTagName("h4").item(0).innerText = itemId + " " + ("\u2BC6".toWellFormed());
+        }
+
     }
 }
 
